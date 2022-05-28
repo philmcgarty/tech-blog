0b0cd11c424b87e9d9d03846561614876cd04a7a
Phil McGarty <philmcgarty@hotmail.com>
2022-05-27 21:34:22 -0400

    added partials and helpers

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
