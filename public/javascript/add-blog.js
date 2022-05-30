f67c692ac41f651db1ab02461526b8e3a3fe13a4
Phil McGarty <philmcgarty@hotmail.com>
2022-05-29 20:05:31 -0400

    minor bug fix

--- public/javascript/add-blog.js
+++ public/javascript/add-blog.js
@@ -4,22 +4,25 @@ async function newFormHandler(event) {
     const title = document.querySelector('input[name="blog-title"]').value;
     const article = document.querySelector('textarea[name="blog-article"]').value;
 
-    const response = await fetch(`/api/blogs`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            article
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+    if (title && article) {
+        const response = await fetch(`/api/blogs`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title,
+                article
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
     }
+
 }
 
 document.querySelector('.new-blog-form').addEventListener('submit', newFormHandler);
